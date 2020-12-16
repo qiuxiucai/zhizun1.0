e217720b758ed6a42291d5361a860487be60fd79
lishuang <1192095876@qq.com>
2020-12-16 15:06:47 +0800

    新增系列分类

---
+++ src/main/java/com/ruoyi/project/product/details/domain/Goods.java
@@ -0,0 +1,38 @@
+package com.ruoyi.project.product.details.domain;
+
+import com.ruoyi.framework.web.domain.BaseEntity;
+import lombok.Data;
+
+@Data
+public class Goods extends BaseEntity {
+    //产品主键
+    private Integer productId;
+    //产品型号(编号)
+    private String productModel;
+    //产品名称
+    private String productName;
+    //产品类型
+    private String productType;
+    //产品图片
+    private String productUrl;
+    //产品系列id外键
+    private Integer productSeriesId;
+    //产品风格id外键
+    private Integer productStyleId;
+    //产品规格id外键
+    private Integer productSizeId;
+    //产品材质
+    private String productMaterials;
+    //产品产地
+    private String productArea;
+    //产品单价
+    private Long productPrice;
+    //产品数量
+    private Integer productNum;
+    //产品总价
+    private Long productTotalPrice;
+    //产品状态
+    private String productStatus;
+    //产品备注
+    private String productRemark;
+}
