741f8ede7a6f9f8ece60b53ae9f92217bc02906e
noobwuxx <1912957846@qq.com>
2020-12-16 20:38:18 +0800

    提交人事的增加和更新功能(更新功能的修改状态有bug)

--- src/main/java/com/ruoyi/project/staff/message/service/IStaffServiceImpl.java
+++ src/main/java/com/ruoyi/project/staff/message/service/IStaffServiceImpl.java
@@ -1,6 +1,10 @@
 package com.ruoyi.project.staff.message.service;
 
+import com.ruoyi.common.exception.BusinessException;
+import com.ruoyi.framework.web.domain.AjaxResult;
+import com.ruoyi.project.staff.message.domain.Dep;
 import com.ruoyi.project.staff.message.domain.Staff;
+import com.ruoyi.project.staff.message.domain.StaffJob;
 import com.ruoyi.project.staff.message.domain.vo.StaffVO;
 import com.ruoyi.project.staff.message.mapper.StaffMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Service("staffService")
 public class IStaffServiceImpl implements IStaffService {
 
     @Autowired
@@ -18,5 +22,54 @@ public class IStaffServiceImpl implements IStaffService {
     public List<StaffVO> selectStaffList(Staff staff) {
 
         return staffMapper.selectStaffList(staff);
+    }
+
+    /**
+     * @return 获取所有的部门信息
+     */
+    @Override
+    public List<Dep> getDeps() {
+        return staffMapper.getDeps();
+    }
+
+    @Override
+    public List<StaffJob> getJobs() {
+        return staffMapper.getJobs();
+    }
+
+    @Override
+    public AjaxResult addStaff(Staff staff) {
+
+        int i = staffMapper.addStaff(staff);
+        if (i > 0){
+            return AjaxResult.success("添加员工成功");
+        }
+        return AjaxResult.error("添加员工失败");
+    }
+
+    @Override
+    public AjaxResult editStaff(Staff staff) {
+        int i = staffMapper.editStaff(staff);
+        if (i > 0){
+            return AjaxResult.success("修改员工成功");
+        }
+        return AjaxResult.error("修改员工失败");
+    }
+
+    @Override
+    public List<Staff> getStaffByDepId(Long depId) {
+        return staffMapper.getStaffByDepId(depId);
+    }
+
+    @Override
+    public String checkSJobNumUnique(Staff staff) {
+
+        return String.valueOf(staffMapper.checkSJobNumUnique(staff));
+    }
+
+    @Override
+    public Staff selectStaffByJobNum(Long jId) {
+
+        return staffMapper.selectStaffByJobNum(jId);
     }
 }
